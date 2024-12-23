5cd440818a59704783aec8aeb661441e0d232ab3
khuedn <khuedn@sonat.vn>
2024-12-23 17:26:17 +0700

    fix: remove unecessary file

--- src/lib/firebase/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBXWeGRo206SoKZXtzqOkJ4jC3Bpypst3E",
-  authDomain: "sonat-bi-help-center.firebaseapp.com",
-  projectId: "sonat-bi-help-center",
-  storageBucket: "sonat-bi-help-center.firebasestorage.app",
-  messagingSenderId: "4111122358",
-  appId: "1:4111122358:web:daa841dafbba17863d4b17",
-  measurementId: "G-84HCNYCLVG",
-};
